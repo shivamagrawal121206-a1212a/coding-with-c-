a1d8526b810b6d896d699cba26659c2e86b24e92
Shivam Agrawal <shivam.agrawal121206@gmail.com>
2026-09-22 22:09:23 +0530

    Add files via upload

---
+++ c++ coding.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int arr[100],n;
+cout<<"Enter the size of array";
+cin>>n;
+cout<<"Enter "<<n<<" elements";
+for(int i=0;i<n;i++)
+cin>>arr[i];
+cout<<"Enter the new value";
+int val;
+cin>>val;
+cout<<"Enter the position";
+int pos;
+cin>>pos;
+for(int i=n-1; i>=pos-1; i--)
+arr[i+1]=arr[i];
+arr[pos-1]=val;
+cout<<"Elements after insertion";
+for(int i=0; i<=n; i++)
+cout<<arr[i]<<" ";
+return 0;
+}
